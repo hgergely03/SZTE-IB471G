1e466eea96c84354ba3d6ac2cda138b0fb7615c0
hgergely03 <gergely0703@gmail.com>
2024-04-20 22:55:30 +0200

    Create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'upload', component: UploadComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: 'details/:id', component: DetailsComponent},
+    {path: '**', redirectTo: ''}
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+    login = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+
+    register = new FormGroup({
+        username: new FormControl('', [Validators.required, Validators.minLength(5)]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+
+    getErrorMessage(field: FormControl): string | null {
+        const errors = field.errors;
+        if (errors?.['required']) {
+            return 'Field is required!';
+        }
+
+        if (errors?.['email']) {
+            return 'Email is invalid!';
+        }
+
+        if (errors?.['minlength']) {
+            return `Field must be at least ${errors['minlength'].requiredLength} characters!`;
+        }
+
+        return null;
+    }
+}
