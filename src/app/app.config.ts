22753035147ba98aa839bcdf0a07384a4bc8963e
hgergely03 <gergely0703@gmail.com>
2024-05-11 09:24:41 +0200

    Setup Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage()))]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        "projectId": "szte-webkeret",
+        "appId": "1:167780338016:web:998644fad7142fb0aa3605",
+        "storageBucket": "szte-webkeret.appspot.com",
+        "apiKey": "AIzaSyBrU6c8xX9FiKTB-6htDk_ZpIea85iEJhU",
+        "authDomain": "szte-webkeret.firebaseapp.com",
+        "messagingSenderId": "167780338016"
+    }
+};
